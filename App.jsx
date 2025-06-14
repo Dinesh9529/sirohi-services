7e89c375b83a8013715ac81acd485144e8e062ba
Dinesh9529 <121393084+Dinesh9529@users.noreply.github.com>
2025-06-14 14:12:21 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CustomerLogin from './pages/CustomerLogin';
+import Services from './components/Services';
 
 function App() {
   return (
     <div>
-      <h2>Sirohi OTP Login Page</h2>
+      <h1>Sirohi Services</h1>
       <CustomerLogin />
+      <Services />
     </div>
   );
 }
